4eb79a3f7904cb8597af8b653dbe9b0f7ae6ed68
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-31 14:54:58 +0330

    init: server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,27 @@
 package server
+
+import (
+	"flag"
+	"os"
+)
+
+var (
+	addr = flag.String("address", os.Getenv("PORT"), "")
+	cert = flag.String("cert", "", "")
+	key  = flag.String("key", "", "")
+)
+
+func Run() error {
+	flag.Parse()
+
+	if *addr == ":" {
+		*addr = ":8080"
+	}
+
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create", handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", )
+	app.Get("/room/:uuid/chat", handlers.RoomChat)
+	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+}
