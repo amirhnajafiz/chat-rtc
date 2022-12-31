d97f6b5a57bfd19c6c42e0dcf696f46861963c8e
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-31 14:57:45 +0330

    feat: complete routes

--- internal/server/server.go
+++ internal/server/server.go
@@ -21,7 +21,12 @@ func Run() error {
 	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket", )
+	app.Get("/room/:uuid/websocket")
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
+	app.Get("/stream/:ssuid", handlers.Stream)
+	app.Get("/stream/:ssuid/websocket")
+	app.Get("/stream/:ssuid/chat/websocket")
+	app.Get("/stream/:ssuid/viewer/websocket")
 }
