00e7ac33660eef9222377619b5f312e3b6a21666
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-04 14:22:22 +0330

    init: main

--- cmd/http.go
+++ cmd/http.go
@@ -10,13 +10,13 @@ import (
 type HTTP struct{}
 
 // Command returns the cobra command of HTTP.
-func (h *HTTP) Command() *cobra.Command {
+func (h HTTP) Command() *cobra.Command {
 	run := func(cmd *cobra.Command, args []string) { h.main() }
 
 	return &cobra.Command{Use: "execute http server", Run: run}
 }
 
 // main is the main function of HTTP.
-func (h *HTTP) main() {
+func (h HTTP) main() {
 	log.Println("http server started")
 }
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"github.com/amirhnajafiz/churchill/cmd"
+	"github.com/spf13/cobra"
+)
+
 // main function of Churchill application.
 // in this function we start the application using cobra.
 func main() {
+	// create the root command
+	root := &cobra.Command{}
 
+	// add cmd commands
+	root.AddCommand(
+		cmd.HTTP{}.Command(),
+	)
+
+	// execute root command
+	if err := root.Execute(); err != nil {
+		panic(err)
+	}
 }
