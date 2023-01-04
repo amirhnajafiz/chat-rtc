f7ad720c6925ec064de9a282591e25471694732d
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-04 14:19:44 +0330

    add: http command

--- cmd/http.go
+++ cmd/http.go
@@ -1,1 +1,22 @@
 package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// HTTP manages the execution of http server for Churchill.
+type HTTP struct{}
+
+// Command returns the cobra command of HTTP.
+func (h *HTTP) Command() *cobra.Command {
+	run := func(cmd *cobra.Command, args []string) { h.main() }
+
+	return &cobra.Command{Use: "execute http server", Run: run}
+}
+
+// main is the main function of HTTP.
+func (h *HTTP) main() {
+	log.Println("http server started")
+}
