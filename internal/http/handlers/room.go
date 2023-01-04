1caaf89bab9c90bacbd5520236510a2165edf24d
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-04 19:39:24 +0330

    add: room websocket method

--- internal/http/handlers/room.go
+++ internal/http/handlers/room.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket"
 	"github.com/google/uuid"
 )
 
 // RoomCreate will send the user to a new room.
 func (h *Handler) RoomCreate(c *fiber.Ctx) error {
 	return c.Redirect(fmt.Sprintf("/room/%s", uuid.New().String()))
+}
+
+// RoomWebsocket .
+func (h *Handler) RoomWebsocket(c *websocket.Conn) {
+	id := c.Params("uuid")
+	if id == "" {
+		return
+	}
+
+	createOrGetRoom(id)
 }
