9550ed03e88f23e4b9ffea319765b75b4525a9a7
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-04 23:26:09 +0330

    add: room route

--- internal/http/handlers/room.go
+++ internal/http/handlers/room.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
@@ -11,6 +12,16 @@ import (
 // RoomCreate will send the user to a new room.
 func (h *Handler) RoomCreate(c *fiber.Ctx) error {
 	return c.Redirect(fmt.Sprintf("/room/%s", uuid.New().String()))
+}
+
+func (h *Handler) Room(c *fiber.Ctx) error {
+	id := c.Params("uuid")
+	if id == "" {
+		c.Status(http.StatusBadRequest)
+		return nil
+	}
+
+	suuid, uuid, _ := createOrGetRoom(id)
 }
 
 // RoomWebsocket creates or gets a room.
