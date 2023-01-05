e7c42fe49ac1ad703cf030744df83d8b58c2a993
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-05 18:26:43 +0330

    feat: all hub methods

--- pkg/chat/hub.go
+++ pkg/chat/hub.go
@@ -6,3 +6,34 @@ type Hub struct {
 	register   chan *Client
 	unregister chan *Client
 }
+
+func NewHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.Send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+				}
+			}
+		}
+	}
+}
