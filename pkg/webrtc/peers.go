c558d92779b31d54be4bab62845ed623a6a240ad
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-05 18:02:56 +0330

    init: webrtc peers

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -1,1 +1,17 @@
 package webrtc
+
+import "sync"
+
+type Room struct {
+	Peers Peers
+}
+
+type Peers struct {
+	ListLock    sync.RWMutex
+	Connections []PeerConnectionState
+	TrackLocals map[string]webrtc.TrackLocalStaticRTP
+}
+
+func (p *Peers) DispatchKeyFrame() {
+
+}
