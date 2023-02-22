c47a442e116aa782ba5b1a440da66148133d7512
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-22 20:08:24 +0330

    init: chat handler

--- internal/http/handlers/chat.go
+++ internal/http/handlers/chat.go
@@ -1,1 +1,18 @@
 package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+)
+
+func (h *Handler) RoomChat(c *fiber.Ctx) error {
+	return c.Render("chat", fiber.Map{}, "layouts/main")
+}
+
+func (h *Handler) RoomChatWebsocket(c *websocket.Conn) {
+
+}
+
+func (h *Handler) StreamChatWebsocket(c *websocket.Conn) {
+
+}
