2ac3712a3d3bda785fa75913303f77e17531087f
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-23 09:06:24 +0330

    add: app

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,5 +1,29 @@
 package app
 
-func New() {
+import (
+	"github.com/amirhnajafiz/chat-rtc/internal/handler/http"
+	"github.com/amirhnajafiz/chat-rtc/internal/handler/socket"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
 
+func New() {
+	r := gin.Default()
+
+	ws := socket.WebSocket{
+		Melody: melody.New(),
+	}
+	ws.Melody.HandleMessage(ws.MessageHandler)
+
+	h := http.Handler{
+		WS: &ws,
+	}
+
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+	r.GET("/ws", h.OpenSocket)
+
+	if err := r.Run(":5000"); err != nil {
+		panic(err)
+	}
 }
