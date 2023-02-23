bca1ba88da80da91854e54d378b172da2f1f9e57
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-23 09:03:43 +0330

    add: http handler

--- internal/handler/http/handler.go
+++ internal/handler/http/handler.go
@@ -1,4 +1,16 @@
 package http
 
+import (
+	"github.com/amirhnajafiz/chat-rtc/internal/handler/socket"
+	"github.com/gin-gonic/gin"
+)
+
 type Handler struct {
+	WS *socket.WebSocket
+}
+
+func (h *Handler) OpenSocket(c *gin.Context) {
+	if err := h.WS.Melody.HandleRequest(c.Writer, c.Request); err != nil {
+		_ = c.Error(err)
+	}
 }
